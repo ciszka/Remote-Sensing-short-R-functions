116d33a437c493761bf34a72cbcc222c76376cba
P.F. Przystupa <ciszka@unm.edu>
2017-04-04 18:01:59 -0600

    Update NDVI.R

--- NDVI.R
+++ NDVI.R
@@ -1,3 +1,7 @@
+#This calculates NDVI on an image that has multiple bands of imagery
+#It should be given a rasterstack with the different bands
+#i and k indicate the band numbers that should be use (almost always band 3 and 4 but 
+#It may vary dependin gon the imagery
 NDVI = function(img, i, k){
   bi = img[[i]]
   bk = img[[k]]
