f0c51d407b68c7e328f39ba55b2ede527cd77583
P.F. Przystupa <ciszka@unm.edu>
2017-04-04 18:10:18 -0600

    Update simpleRatio.R

--- simpleRatio.R
+++ simpleRatio.R
@@ -1,3 +1,9 @@
+#This calculates a simple ratio on an image that has multiple bands of imagery
+#It should be given a rasterstack with the different bands and is basd on the Simple
+#Ratio formula in Jensen 2016. 
+#Any band number can be used in this function but be aware of what sort of information
+#it actually produces at the end and what properties are being tested
+
 simpleRatio = function(img, band1, band2){
   bi = img[[band1]]
   bk = img[[band2]]
