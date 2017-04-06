5452266e1c4bce49ce9fb32cdc3cf4eeeff0bb07
P.F. Przystupa <ciszka@unm.edu>
2017-04-06 16:05:57 -0600

    Update createAllNDVIMasksFromLocation.R

--- createAllNDVIMasksFromLocation.R
+++ createAllNDVIMasksFromLocation.R
@@ -2,11 +2,23 @@
 #that are stored within a particular file
 #The expansion of this is the reclassVegSoil
 #I should have written documenation for this previously
+#fileLocation should be the full file path from drive to folder
+#Please make sure that the correct slashes are used \ need to be changed to /
+#For the program to interpret it correctly
 createAllNDVIMasksFromLocation = function(fileLocation, j, k){
-  filesToAnalyze = list.files(fileLocation)
-  numberOfFiles = length(filesToAnalyze)
+  filesToAnalyze = list.files(fileLocation) # lists the files in the location given to be used in the analysis
+  numberOfFiles = length(filesToAnalyze) #Lists the number of files so the for loop doesn't go past it for iterations
+  
+  #This is the most confusing part for reclassification in R a matrix of the values needs to be created
+  #This is not the most elegant version to solve this but its what I ended up using
+  #All values for the matrix are stored in this vector in a column sequential format
+  #All values from column, followed by all the values for column 2 etc.
   matrixValues = c(-1,.3, .9 , .3 ,.9, 1, 1,0, 1)
+  
+  #This actually turns the vecor into a matrix, in this case a 3x3 since I'm separating the vegetation and non
+  #Vegetation around the value, these may be changed to separate into whatever NDVI class values you have listed
   categories = matrix(matrixValues, nrow = 3, ncol=3)
+  
   colnames(categories) <- c('from', 'to', 'becomes')
   
   
