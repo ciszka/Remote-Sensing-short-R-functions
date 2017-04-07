1b7db8afdae7ddd7bddeeddfad7b53fdf1900dc5
P.F. Przystupa <ciszka@unm.edu>
2017-04-07 17:37:00 -0600

    Update createAllNDVIMasksFromLocation.R

--- createAllNDVIMasksFromLocation.R
+++ createAllNDVIMasksFromLocation.R
@@ -2,40 +2,41 @@
 #that are stored within a particular file
 #The expansion of this is the reclassVegSoil
 #I should have written documenation for this previously
-#fileLocation should be the full file path from drive to folder
-#Please make sure that the correct slashes are used \ need to be changed to /
-#For the program to interpret it correctly
+#Updated from previous version so it only works  with tifs for now
 createAllNDVIMasksFromLocation = function(fileLocation, j, k){
-  filesToAnalyze = list.files(fileLocation) # lists the files in the location given to be used in the analysis
-  numberOfFiles = length(filesToAnalyze) #Lists the number of files so the for loop doesn't go past it for iterations
+  #Required libraries
+  require(utils)
+  require(raster)
   
-  #This is the most confusing part for reclassification in R a matrix of the values needs to be created
-  #This is not the most elegant version to solve this but its what I ended up using
-  #All values for the matrix are stored in this vector in a column sequential format
-  #All values from column, followed by all the values for column 2 etc.
+  #Deals withwhere to find the files and which files to include
+  filesToAnalyze = list.files(fileLocation)
+  numberOfFiles = length(filesToAnalyze)
+  #Values for the reclassification of them to create a mask
   matrixValues = c(-1,.3, .9 , .3 ,.9, 1, 1,0, 1)
-  
-  #This actually turns the vecor into a matrix, in this case a 3x3 since I'm separating the vegetation and non
-  #Vegetation around the value, these may be changed to separate into whatever NDVI class values you have listed
   categories = matrix(matrixValues, nrow = 3, ncol=3)
-  
   colnames(categories) <- c('from', 'to', 'becomes')
   
-  
+  #This actually cycles through all the data to do the NDVI calculation and the mask
   for (i in 1:numberOfFiles) {
-    fullFileName = paste(fileLocation, filesToAnalyze[i], sep = "/", collapse = "")
-    img = brick(fullFileName)
-    noExt = strtrim(fullFileName, nchar(fullFileName)-4)
-    newFileName = paste(noExt, "_NDVI", sep = "", collapse = "")
-    print(newFileName)
-    bj = img[[j]]
-    bk = img[[k]]
-    NDVI = (bk-bj)/(bk+bj)
-    writeRaster(NDVI, filename = newFileName, format = 'GTiff', overwrite = TRUE)
-    NDVIFile = brick(NDVI)
-    maskName = paste(newFileName, "_mask", sep = "", collapse = "")
-    reclassedImage = reclassify(NDVIFile, categories)
-    writeRaster(reclassedImage, filename = maskName, format = 'GTiff', overwrite = TRUE)
+    ext = substr(filesToAnalyze[i], nchar(filesToAnalyze[i])-2, nchar(filesToAnalyze[i])) #This just gets the extension of the piece
+    #Tests if its the file type we want
+    if (ext == "tif" ||ext == "TIF") {
+      fullFileName = paste(fileLocation, filesToAnalyze[i], sep = "/", collapse = "")
+      img = brick(fullFileName)
+      noExt = strtrim(fullFileName, nchar(fullFileName)-4)
+      newFileName = paste(noExt, "_NDVI", sep = "", collapse = "")
+      print(newFileName)
+      bj = img[[j]]
+      bk = img[[k]]
+      NDVI = (bk-bj)/(bk+bj)
+      writeRaster(NDVI, filename = newFileName, format = 'GTiff', overwrite = TRUE)
+      NDVIFile = brick(NDVI)
+      maskName = paste(newFileName, "_mask", sep = "", collapse = "")
+      reclassedImage = reclassify(NDVIFile, categories)
+      writeRaster(reclassedImage, filename = maskName, format = 'GTiff', overwrite = TRUE)
+    } else {
+      print ("It wasn't my type")
+    }
   }  
   return("did the thing")  
 }
